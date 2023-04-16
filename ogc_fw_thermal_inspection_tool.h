567cbbb3d9114b5c7227f2b740938db2e71f9699
Dustin <dustin@ogc.engineering>
2023-04-15 23:08:38 -0500

    WIP source formatting and planning

--- ogc_fw_thermal_inspection_tool.h
+++ ogc_fw_thermal_inspection_tool.h
@@ -3,9 +3,42 @@
 
 #include <stdint.h>
 
-void hal_config( void );
-void hal_init( void );
+/* =====================================================================================================================
+ *                                                                         OGC.Engineering Layered Development Standards
+ * =====================================================================================================================
+ */
 
-uint64_t hal_get_sys_tick( void );
+void hal_config( void ); /* ogc layered development standard - hardware configuration call */
+void hal_init( void ); /* ogc layered development standard - hardware init call */
+
+uint64_t hal_get_sys_tick( void ); /* ogc layered development standard - monotonic system tick */
+
+/* =====================================================================================================================
+ *                                                                                              General System Functions
+ * =====================================================================================================================
+ */
+/*                                                                                                  Clocks - Watchdog */
+/*                                                                                                      Clocks - RTCC */
+/*                                                                                                    Clocks - System */
+
+/* =====================================================================================================================
+ *                                                                                                        User Interface
+ * =====================================================================================================================
+ */
+/*                                                                          User Interface - RGB LED ( GPIO and PWM ) */
+/*                                                                        User Interface - Encoder Phase A ( Button ) */
+/*                                                                        User Interface - Encoder Phase B ( Button ) */
+/*                                                                          User Interface - Encoder Click ( Button ) */
+/*                                                                                      User Interface - Serial Comms */
+/*                                                                                      User Interface - OLED Display */
+
+/* =====================================================================================================================
+ *                                                                                                               Sensors
+ * =====================================================================================================================
+ */
+/*                                                                                               Sensors - RGB Camera */
+/*                                                                                                Sensors - IR Camera */
+/*                                                                                                    Sensors - LIDAR */
+
 
 #endif /* OGC_FW_THERMAL_INSPECTION_TOOL_H */
