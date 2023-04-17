c12f8ad0f2b830342504fca9a6112fdc9194e29f
Dustin <dustin@ogc.engineering>
2023-04-16 19:26:00 -0500

    WIP source and header updates not tested

--- ogc_fw_thermal_inspection_tool.c
+++ ogc_fw_thermal_inspection_tool.c
@@ -82,46 +82,15 @@ Timer1IntHandler(void)
 
 void hal_config( void )
 {
+    watchdog_stop();
+    watchdog_init();
+    watchdog_start();
+
     sys_tick = 0U;
 
-    //
-    // Set the clocking to run directly from the crystal.
-    //
-    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
-                       SYSCTL_XTAL_16MHZ);
+    system_clock_config();
 
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-
-
-    //
-    // Check if the peripheral access is enabled.
-    //
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
-    {
-    }
-
-    //
-    // Enable the GPIO pins for the LED_RED.
-    //
-    GPIOPinTypeGPIOOutput( HW_RGB_RED_PORT, HW_RGB_RED_PIN );
-
-    //
-    // Enable the GPIO pins for the LED_BLUE.
-    //
-    GPIOPinTypeGPIOOutput( HW_RGB_BLUE_PORT, HW_RGB_BLUE_PIN );
-
-    //
-    // Enable the GPIO pins for the LED_GREEN.
-    //
-    GPIOPinTypeGPIOOutput( HW_RGB_GREEN_PORT, HW_RGB_GREEN_PIN );
-
-
-    GPIOPinWrite( HW_RGB_RED_PORT, HW_RGB_RED_PIN, 0x0 );
-    GPIOPinWrite( HW_RGB_BLUE_PORT, HW_RGB_BLUE_PIN, 0x0 );
-    GPIOPinWrite( HW_RGB_GREEN_PORT, HW_RGB_GREEN_PIN, 0x0 );
+    ui_status_indicator_init_as_gpio();
 
 }
 
@@ -134,3 +103,185 @@ uint64_t hal_get_sys_tick( void )
 {
     return( sys_tick );
 }
+
+/* =====================================================================================================================
+ *                                                                                              General System Functions
+ * =====================================================================================================================
+ */
+/*                                                                                                  Clocks - Watchdog */
+void watchdog_init( void ) /* configure watchdog clock */
+{
+    
+}
+
+void watchdog_stop( void ) /* stop watchdog clock */
+{
+    
+}
+
+void watchdog_start( void ) /* start watchdog clock */
+{
+    
+}
+
+void watchdog_reset( void ) /* reset watchdog clock */
+{
+    
+}
+
+/*                                                                                                      Clocks - RTCC */
+/* configure rtcc clock */
+/* start rtcc clock */
+/* stop rtcc clock */
+/* set rtcc clock */
+/* get rtcc clock */
+
+/*                                                                                                    Clocks - System */
+void system_clock_config( void ) /* configure system clock */
+{
+        SysCtlClockSet( SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ );
+}
+
+/* enter low power mode */
+
+/* init I2C module */
+
+/* init SPI module */
+
+/* =====================================================================================================================
+ *                                                                                                        User Interface
+ * =====================================================================================================================
+ */
+/*                                                                          User Interface - RGB LED ( GPIO and PWM ) */
+void ui_status_indicator_init_as_gpio( void ) /* init rgb for gpio operation */
+{
+    SysCtlPeripheralEnable( HW_RGB_RED_PORT | HW_RGB_GREEN_PORT | HW_RGB_BLUE_PORT );
+    while( false == SysCtlPeripheralReady( HW_RGB_RED_PORT | HW_RGB_GREEN_PORT | HW_RGB_BLUE_PORT ) );
+    GPIOPinTypeGPIOOutput( HW_RGB_RED_PORT, HW_RGB_RED_PIN );
+    ui_status_indicator_red_set_gpio( false );
+    GPIOPinTypeGPIOOutput( HW_RGB_GREEN_PORT, HW_RGB_GREEN_PIN );
+    ui_status_indicator_green_set_gpio( false );
+    GPIOPinTypeGPIOOutput( HW_RGB_BLUE_PORT, HW_RGB_BLUE_PIN );
+    ui_status_indicator_blue_set_gpio( false );
+}
+
+void ui_status_indicator_red_set_gpio( bool state ) /* set red state */
+{
+    if ( true == state )
+    {
+        GPIOPinWrite( HW_RGB_RED_PORT, HW_RGB_RED_PIN, HW_RGB_RED_PIN );
+    }
+    else
+    {
+        GPIOPinWrite( HW_RGB_RED_PORT, HW_RGB_RED_PIN, 0x0 );
+    }
+}
+
+void ui_status_indicator_green_set_gpio( bool state ) /* set green state */
+{
+    if ( true == state )
+    {
+        GPIOPinWrite( HW_RGB_GREEN_PORT, HW_RGB_GREEN_PIN, HW_RGB_GREEN_PIN );
+    }
+    else
+    {
+        GPIOPinWrite( HW_RGB_GREEN_PORT, HW_RGB_GREEN_PIN, 0x0 );
+    }
+}
+
+void ui_status_indicator_blue_set_gpio( bool state ) /* set blue state */
+{
+    if ( true == state )
+    {
+        GPIOPinWrite( HW_RGB_BLUE_PORT, HW_RGB_BLUE_PIN, HW_RGB_BLUE_PIN );
+    }
+    else
+    {
+        GPIOPinWrite( HW_RGB_BLUE_PORT, HW_RGB_BLUE_PIN, 0x0 );
+    }
+}
+
+void ui_status_indicator_init_as_pwm( void ) /* init rgb for pwm operation */
+{
+    
+}
+
+/* set red pwm */
+/* set green pwm */
+/* set blue pwm */
+
+/*                                                                        User Interface - Encoder Phase A ( Button ) */
+/* init phase a as input with interrupt */
+/* get phase a current state */
+
+/*                                                                        User Interface - Encoder Phase B ( Button ) */
+/* init phase b as input with interrupt */
+/* get phase b current state */
+
+/*                                                                          User Interface - Encoder Click ( Button ) */
+/* init click as input with interrupt */
+/* get click current state */
+
+/*                                                                                      User Interface - Serial Comms */
+/* init serial uart to give baud rate, etc. */
+/* get char */
+/* get string
+/* put char */
+/* put string */
+
+/*                                                                                      User Interface - OLED Display */
+/* init oled select pin */
+/* set oled select pin */
+/* init oled data type pin */
+/* set oled data type pin */
+/* init oled reset pin */
+/* set oled reset pin */
+/* send command to oled */
+/* send data to oled */
+
+/*                                                                                           User Interface - SD Card */
+/* init sd card select pin */
+/* get sd card select pin */
+/* set sd card select pin */
+/* init sd card detect pin */
+/* get sd card detect state */
+
+/* =====================================================================================================================
+ *                                                                                                               Sensors
+ * =====================================================================================================================
+ */
+/*                                                                                               Sensors - RGB Camera */
+/* init camera color pin data port */
+/* get camera color state data port */
+/* set camera color state data port */
+/* init camera color pin vsync */
+/* get camera color state vsync */
+/* set camera color state vsync */
+/* init camera color pin href */
+/* get camera color state href */
+/* set camera color state href */
+/* init camera color pin pclk */
+/* get camera color state pclk */
+/* set camera color state pclk */
+/* init camera color pin xclk */
+/* get camera color state xclk */
+/* set camera color state xclk */
+/* init camera color pin powerdown */
+/* get camera color state powerdown */
+/* set camera color state powerdown */
+/* init camera color pin reset */
+/* get camera color state reset */
+/* set camera color state reset */
+/* i2c/spi camera color send command */
+
+/*                                                                                                Sensors - IR Camera */
+/* i2c camera ir send command */
+
+/*                                                                                                    Sensors - LIDAR */
+/* init lidar mode pin */
+/* get lidar mode state */
+/* set lidar mode state */
+/* init lidar enable pin */
+/* get lidar enable state */
+/* set lidar enable state */
+/* i2c lidar send command */
--- ogc_fw_thermal_inspection_tool.h
+++ ogc_fw_thermal_inspection_tool.h
@@ -18,27 +18,115 @@ uint64_t hal_get_sys_tick( void ); /* ogc layered development standard - monoton
  * =====================================================================================================================
  */
 /*                                                                                                  Clocks - Watchdog */
+void watchdog_init( void ); /* configure watchdog clock */
+void watchdog_stop( void ); /* stop watchdog clock */
+void watchdog_start( void ); /* start watchdog clock */
+void watchdog_reset( void ); /* reset watchdog clock */
+
 /*                                                                                                      Clocks - RTCC */
+/* configure rtcc clock */
+/* start rtcc clock */
+/* stop rtcc clock */
+/* set rtcc clock */
+/* get rtcc clock */
+
 /*                                                                                                    Clocks - System */
+/* configure system clock */
+/* enter low power mode */
+
+/* init I2C module */
+
+/* init SPI module */
 
 /* =====================================================================================================================
  *                                                                                                        User Interface
  * =====================================================================================================================
  */
 /*                                                                          User Interface - RGB LED ( GPIO and PWM ) */
+void ui_status_indicator_init_as_gpio( void ); /* init rgb for gpio operation */
+void ui_status_indicator_red_set_gpio( bool state ); /* set red state */
+void ui_status_indicator_green_set_gpio( bool state ); /* set green state */
+void ui_status_indicator_blue_set_gpio( bool state ); /* set blue state */
+
+/* init rgb for pwm operation */
+/* set red pwm */
+/* set green pwm */
+/* set blue pwm */
+
 /*                                                                        User Interface - Encoder Phase A ( Button ) */
+/* init phase a as input with interrupt */
+/* get phase a current state */
+
 /*                                                                        User Interface - Encoder Phase B ( Button ) */
+/* init phase b as input with interrupt */
+/* get phase b current state */
+
 /*                                                                          User Interface - Encoder Click ( Button ) */
+/* init click as input with interrupt */
+/* get click current state */
+
 /*                                                                                      User Interface - Serial Comms */
+/* init serial uart to give baud rate, etc. */
+/* get char */
+/* get string
+/* put char */
+/* put string */
+
 /*                                                                                      User Interface - OLED Display */
+/* init oled select pin */
+/* set oled select pin */
+/* init oled data type pin */
+/* set oled data type pin */
+/* init oled reset pin */
+/* set oled reset pin */
+/* send command to oled */
+/* send data to oled */
+
+/*                                                                                           User Interface - SD Card */
+/* init sd card select pin */
+/* get sd card select pin */
+/* set sd card select pin */
+/* init sd card detect pin */
+/* get sd card detect state */
 
 /* =====================================================================================================================
  *                                                                                                               Sensors
  * =====================================================================================================================
  */
 /*                                                                                               Sensors - RGB Camera */
-/*                                                                                                Sensors - IR Camera */
-/*                                                                                                    Sensors - LIDAR */
+/* init camera color pin data port */
+/* get camera color state data port */
+/* set camera color state data port */
+/* init camera color pin vsync */
+/* get camera color state vsync */
+/* set camera color state vsync */
+/* init camera color pin href */
+/* get camera color state href */
+/* set camera color state href */
+/* init camera color pin pclk */
+/* get camera color state pclk */
+/* set camera color state pclk */
+/* init camera color pin xclk */
+/* get camera color state xclk */
+/* set camera color state xclk */
+/* init camera color pin powerdown */
+/* get camera color state powerdown */
+/* set camera color state powerdown */
+/* init camera color pin reset */
+/* get camera color state reset */
+/* set camera color state reset */
+/* i2c/spi camera color send command */
 
+/*                                                                                                Sensors - IR Camera */
+/* i2c camera ir send command */
+
+/*                                                                                                    Sensors - LIDAR */
+/* init lidar mode pin */
+/* get lidar mode state */
+/* set lidar mode state */
+/* init lidar enable pin */
+/* get lidar enable state */
+/* set lidar enable state */
+/* i2c lidar send command */
 
 #endif /* OGC_FW_THERMAL_INSPECTION_TOOL_H */
